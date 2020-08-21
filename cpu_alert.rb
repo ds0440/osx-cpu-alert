afb786f52ad194621c7d980fd88d0a37866b40b1
David Bryant <david.bryant@cfacorp.com>
2020-08-21 19:01:28 -0400

    added logger, updated format, collects more data

--- cpu_alert.rb
+++ cpu_alert.rb
@@ -3,11 +3,21 @@
 max_load = 3
 
 load = `sysctl -n vm.loadavg`
-puts load
-load_5min = load.split(" ")[2].to_f
-puts load_5min
-if load_5min > max_load
-  ps1 = `ps aux -r`.split("\n")[1].split(" ")
-  ps1_basename = File.basename(ps1.last)
-  `osascript -e 'display notification "#{load_5min} load > #{max_load} max\n#{ps1[2]}% #{ps1_basename}" with title "High Load"'`
+# One minute average
+# move to [2] for 5 minute average
+# I didn't want to miss anything so I am loading info for the top few procs
+load_1min = load.split(" ")[1].to_f
+if load_1min > max_load
+  psr = `ps aux -r`.split("\n")
+  ps1_raw = psr[1]
+  ps2_raw = psr[2]
+  ps3_raw = psr[3]
+  ps4_raw = psr[5]
+  ps5_raw = psr[6]
+  ps1_split = ps1_raw.split(" ")
+  ps1_basename = File.basename(ps1_split.last)
+  f = File.new("#{Dir.home}/Desktop/ps_log.txt", "a")
+  f.write("\n-----------Load Avg: #{load_1min}------------\n#{ps1_raw}\n#{ps2_raw}\n#{ps3_raw}\n#{ps4_raw}\n#{ps5_raw}\n---------------end-------------------")
+  `osascript -e 'display notification "#{load_1min} load > #{max_load} max\n#{ps1_split[2]}% #{ps1_basename}" with title "High Load"'`
+  
 end
